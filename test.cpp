29323559f6c41362e7047a5da0f10cf9df270b28
xm-tomox <neo.xtomox@gmail.com>
2025-08-03 21:10:20 +0900

    Revert "edit2-1"
    This reverts commit 57af39daa2e9e8a5c10adf50440d5ea883b1775c.
    
    rev 2

--- test.cpp
+++ test.cpp
@@ -10,7 +10,7 @@ unique_ptr<std::string> createString(const char str[]) {
 
 int main() {
     unique_ptr<string> ptr;
-    ptr = createString("123 ABCDEFG あいう　えお");
+    ptr = createString("ABCDEFG");
     cout << *ptr << endl;
     
     return 0;
