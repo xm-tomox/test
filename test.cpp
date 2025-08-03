57af39daa2e9e8a5c10adf50440d5ea883b1775c
xm-tomox <neo.xtomox@gmail.com>
2025-08-03 20:41:36 +0900

    edit2-1

--- test.cpp
+++ test.cpp
@@ -10,7 +10,7 @@ unique_ptr<std::string> createString(const char str[]) {
 
 int main() {
     unique_ptr<string> ptr;
-    ptr = createString("ABCDEFG");
+    ptr = createString("123 ABCDEFG");
     cout << *ptr << endl;
     
     return 0;
